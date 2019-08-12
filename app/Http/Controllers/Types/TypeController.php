f4b28d69b41ffd01b61e9e1ec84310a573df9fe9
fajarartbekasi <fajarbekasieditting@gmail.com>
2019-08-12 11:30:08 +0700

    add edit and update jenis pinjaman

--- app/Http/Controllers/Types/TypeController.php
+++ app/Http/Controllers/Types/TypeController.php
@@ -60,26 +60,34 @@ class TypeController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * menampilkan form edit jenis pinjaman.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Type $type)
     {
-        //
+        $this->authorize('create', Type::class);
+
+        return view('types.edit', compact('type'));
     }
 
     /**
-     * Update the specified resource in storage.
+     * Update data jenis pinjaman.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(TypesRequest $request, Type $type)
     {
-        //
+        $this->authorize('create', Type::class);
+
+        $type->update($request->all());
+
+        flash('Jenis pinjaman berhasil diperbaharui.');
+
+        return redirect()->route('types.index');
     }
 
     /**
---
+++ resources/views/types/edit.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="col-lg-12">
+
+    <div class="card">
+        <h5 class="card-header">Perbaharui jenis pinjaman</h5>
+        <div class="card-body">
+            <form action="{{route('types.update', $type->id)}}" method="post">
+                @csrf
+                @method('PATCH')
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="nama_jenis_pinjaman">Jenis pinjaman:</label>
+                            <input type="text" name="nama_jenis_pinjaman" value="{{old('nama_jenis_pinjaman', $type->nama_jenis_pinjaman)}}"
+                                id="nama_jenis_pinjaman" class="form-control" placeholder="Example...">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="minimum_jumlah_pinjaman">Minimum jumlah pinjaman:</label>
+                            <input type="number" name="minimum_jumlah_pinjaman"
+                                value="{{old('minimum_jumlah_pinjaman', $type->minimum_jumlah_pinjaman)}}" id="minimum_jumlah_pinjaman"
+                                class="form-control">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="maksimum_jumlah_pinjaman">Maksimum jumlah pinjaman:</label>
+                            <input type="number" name="maksimum_jumlah_pinjaman"
+                                value="{{old('maksimum_jumlah_pinjaman', $type->maksimum_jumlah_pinjaman)}}" id="maksimum_jumlah_pinjaman"
+                                class="form-control">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="minimum_lama_angsuran">Minimum lama angsuran:</label>
+                            <input type="number" name="minimum_lama_angsuran" value="{{old('minimum_lama_angsuran', $type->minimum_lama_angsuran)}}"
+                                id="minimum_lama_angsuran" class="form-control">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="maksimum_lama_angsuran">Maksimum lama angsuran:</label>
+                            <input type="text" name="maksimum_lama_angsuran" value="{{old('maksimum_lama_angsuran', $type->maksimum_lama_angsuran)}}"
+                                id="maksimum_lama_angsuran" class="form-control">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="bunga">Bunga:</label>
+                            <input type="number" name="bunga" value="{{old('bunga', $type->bunga)}}" id="bunga" class="form-control">
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <button type="submit" class="btn btn-outline-primary">
+                            Simpan
+                        </button>
+                        <a href="{{route('types.index')}}" type="submit" class="btn btn-outline-secondary">
+                            Cancel
+                        </a>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
