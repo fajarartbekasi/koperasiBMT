2c3c2556178f4f9de70d7c156ee5e352fb728b7b
fajarartbekasi <fajarbekasieditting@gmail.com>
2019-09-30 19:04:58 +0700

    add schedule on homestead

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('sms:send')
-                 ->everyMinute();
+                 ->everyMonth();
     }
 
     /**
